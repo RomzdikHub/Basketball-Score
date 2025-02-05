2addd8798ab671c8362e7467b72afe5b1aa18527
RomzdikHub <romzdik2006@gmail.com>
2025-02-05 18:12:25 +0200

    Commit initial files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let scoreEl1 = document.getElementById("score-el-1");
+let scoreEl2 = document.getElementById("score-el-2");
+
+let score1 = 0;
+let score2 = 0;
+
+function increment(points, team) {
+    if(team === 'home') {
+        score1 += points;
+        scoreEl1.innerText = score1;
+    } else if(team === 'guest') {
+        score2 += points;
+        scoreEl2.innerText = score2;
+    }
+}   
